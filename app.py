7585afa17d4eb77f435c8882a390a78680c1d39f
Charlie <charles.gerrior@gmail.com>
2018-09-23 15:43:35 -0400

    adds all 3 items

--- app.py
+++ app.py
@@ -6,33 +6,23 @@ from brickseek import *
 from stats import *
 
 app = Flask(__name__, static_folder='../../public', static_url_path='')
+skus = [13033157, 145051970, 10450115]
+
 
 # POST route to handle a new subscription form
 @app.route("/api/query/new", methods=['POST'])
 def new_query():
+  items = []
+  for sku in skus:
+    # We'll wrap this in a try to catch any API
+    # errors that may occur
+    try:
+      item = getStats(getInventory(sku,request.form['zip_code']))
+      items.append(item)
+    except:
+      print("ERROR!")
 
-  # We'll wrap this in a try to catch any API
-  # errors that may occur
-  try:
-    item = getStats(getInventory(10450115,request.form['zip_code']))
-    #print(item)
-  except:
-
-    # Here we may wish to log the API error and send the
-    # customer to an appropriate URL, perhaps including
-    # and error message. See the `error_redirect` and
-    # `compose_errors` functions below.
-    #error_redirect(compose_errors(errors))
-    print("ERROR!")
-
-  return jsonify(item), 201
-
-# A few utility functions for error handling
-def error_redirect(message):
-  redirect('ERROR_URL?errors=' + message)
-
-def compose_errors(errors):
-  ', '.join(e.message for e in errors)
+  return jsonify({'results' : items}), 201
 
 if __name__ == "__main__":
   app.run('127.0.0.1', 9001)
