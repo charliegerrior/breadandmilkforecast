9e768059215728d3314c42f75695dc523bdfbebb
Charlie <charles.gerrior@gmail.com>
2018-09-24 21:51:21 -0400

    improves JSON response format

--- app.py
+++ app.py
@@ -13,18 +13,18 @@ staples = [{'name' : 'milk', 'sku' : 10450115}, {'name' : 'eggs', 'sku' : 145051
 # POST route to handle a new form
 @app.route("/api/query/new", methods=['POST'])
 def new_query():
-  items = []
+  items = {}
   for staple in staples:
     # We'll wrap this in a try to catch any API
     # errors that may occur
     try:
       bs_results = getStats(getInventory(staple['sku'],request.form['zip_code']))
-      bs_results['name'] = staple['name']
-      items.append(bs_results)
+      items[staple['name']] = bs_results
+      #items.append(bs_results)
     except:
       print("ERROR!")
 
-  return jsonify({'results' : items}), 201
+  return jsonify(items), 201
 
 if __name__ == "__main__":
   app.run('192.168.1.14', 9001)
