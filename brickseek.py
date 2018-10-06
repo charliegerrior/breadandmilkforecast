d825c85669c3a6f55a946b5319265dc86d9ef96d
Charlie <charles.gerrior@gmail.com>
2018-10-06 17:14:45 -0400

    initial commit

--- brickseek.py
+++ brickseek.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 #bread SKU: 13033157
 #eggs SKU: 145051970
@@ -15,8 +16,13 @@ def getInventory(sku, zip):
   rows = table.find_all('div', class_="bsapi-table__row")
   quantities = []
   for row in rows:
-    cells = row.find_all('div', class_="bsapi-table__cell")
-    quantity_cell = cells[1]
+    #print(row)
+    address = row.find_all('address', class_="bsapi-address")[0]
+    distanceMatch = re.search(r'(\d+).?(\d*)\s*(Miles)', address.get_text(), flags=0)
+    if distanceMatch:
+      print(distanceMatch.group())
+    #print(address)
+    quantity_cell = row.find_all('div', class_="bsapi-table__cell")[1]
     quantity = quantity_cell.find('span', class_="bsapi-table__cell-quantity")
     number = [int(s) for s in quantity.get_text().split() if s.isdigit()]
     quantities.append(number[0])
