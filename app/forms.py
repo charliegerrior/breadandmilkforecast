0a7009590c7fe763b8fc7fd69dc12dd4c13ed973
Charlie <charles.gerrior@gmail.com>
2018-10-16 19:21:05 -0400

    initial commit

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class QueryForm(FlaskForm):
+    zipcode = StringField('zip code', validators=[DataRequired()])
+    bread = BooleanField('bread')
+    eggs = BooleanField('eggs')
+    milk = BooleanField('milk')
+    tp = BooleanField('toilet paper')
+    submit = SubmitField('submit')
