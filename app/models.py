245c1326279869d12c7a730d5aad29ee42fc2598
Charlie <charles.gerrior@gmail.com>
2018-10-25 11:02:50 -0400

    initial commit, adds new table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -7,3 +9,26 @@ class User(db.Model):
     region = db.Column(db.String(5), index=True, unique=False)
     def __repr__(self):
         return '<User {}>'.format(self.name)
+
+class Forecast(db.Model):
+  region = db.Column(db.String(5), primary_key=True)
+  timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+  #eggs
+  eggs_inv = db.Column(db.Integer, unique=False)
+  eggs_stores  = db.Column(db.Integer, unique=False)
+  eggs_distance = db.Column(db.Integer, unique=False)
+  #bread
+  bread_inv = db.Column(db.Integer, unique=False)
+  bread_stores  = db.Column(db.Integer, unique=False)
+  bread_distance = db.Column(db.Integer, unique=False)
+  #milk
+  milk_inv = db.Column(db.Integer, unique=False)
+  milk_stores  = db.Column(db.Integer, unique=False)
+  milk_distance = db.Column(db.Integer, unique=False)
+  #tp
+  tp_inv = db.Column(db.Integer, unique=False)
+  tp_stores  = db.Column(db.Integer, unique=False)
+  tp_distance = db.Column(db.Integer, unique=False)
+
+  def __repr__(self):
+    return '<Forecast {}>'.format(self.region)
