1e8aab292ecabc85bb922d4c4be7808843e53978
Charlie <charles.gerrior@gmail.com>
2018-10-26 10:47:52 -0400

    adds working DB update for existing forecasts

--- app/routes.py
+++ app/routes.py
@@ -45,7 +45,7 @@ def forecast():
     items = []
     region = request.args['zip_code']
     db_forecast = Forecast.query.filter_by(region=region).first()
-    if db_forecast is None or datetime.utcnow() - db_forecast.timestamp > timedelta(seconds=1800):
+    if db_forecast is None: 
       #run new query
       print('no recent forecast in database')
       # We'll wrap this in a try to catch any API
@@ -57,6 +57,26 @@ def forecast():
       #write forecast to DB
       db_forecast = Forecast(region = region, eggs_inv = forecast["eggs"]["mean"], eggs_stores = forecast["eggs"]["percent"], eggs_distance = forecast["eggs"]["distance"], bread_inv = forecast["bread"]["mean"], bread_stores = forecast["bread"]["percent"], bread_distance = forecast["bread"]["distance"], milk_inv = forecast["milk"]["mean"], milk_stores = forecast["milk"]["percent"], milk_distance = forecast["milk"]["distance"], tp_inv = forecast["tp"]["mean"], tp_stores = forecast["tp"]["percent"], tp_distance = forecast["tp"]["distance"])
       db.session.add(db_forecast)
+      db.session.commit()
+    elif datetime.utcnow() - db_forecast.timestamp > timedelta(seconds=1800):
+      try:
+        forecast = getForecast(region)
+      except:
+        print("ERROR!") 
+      #update existing forecast
+      db_forecast.timestamp = datetime.utcnow()
+      db_forecast.eggs_inv = forecast["eggs"]["mean"]
+      db_forecast.eggs_stores = forecast["eggs"]["percent"]
+      db_forecast.eggs_distance = forecast["eggs"]["distance"]
+      db_forecast.bread_inv = forecast["bread"]["mean"]
+      db_forecast.bread_stores = forecast["bread"]["percent"]
+      db_forecast.bread_distance = forecast["bread"]["distance"]
+      db_forecast.milk_inv = forecast["milk"]["mean"]
+      db_forecast.milk_stores = forecast["milk"]["percent"]
+      db_forecast.milk_distance = forecast["milk"]["distance"]
+      db_forecast.tp_inv = forecast["tp"]["mean"]
+      db_forecast.tp_stores = forecast["tp"]["percent"]
+      db_forecast.tp_distance = forecast["tp"]["distance"]
       db.session.commit()
     else:
       #retrieve from DB
